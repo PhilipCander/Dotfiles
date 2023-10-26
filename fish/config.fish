01a184d7cc1a0b148e6f96cb1d21a62d5eb6c9e6
PhilipCander <cander@gmx.de>
2023-10-26 19:45:48 +0000

    Added configs

---
+++ fish/config.fish
@@ -0,0 +1,22 @@
+if status is-interactive
+    	# Commands to run in interactive sessions can go here
+	clear
+	neofetch
+end
+
+set fish_greeting ""
+
+
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias c..="cd ..;clear"
+alias c...="cd../..;clear"
+alias c....="cd../../..;clear"
+alias vim="nvim"
+alias cl="clear;ls;pwd"
+alias c="clear"
+alias e="clear;exit"
+alias ls="ls --color -a"
+alias cdc="cd;clear;pwd;ls"
+
